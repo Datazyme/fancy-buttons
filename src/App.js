9f15d66a9bc4d9c52abb395097e3256cdf7cfbdf
Copernicus <copernicus@CopernicussMini.hitronhub.home>
2023-02-20 20:34:54 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React, {useState} from "react";
+import AngryButton from './components/AngryButton';
+import CounterButton from './components/CounterButton';
+import LightSwitchButton from './components/LightSwitchButton';
+import TextRepeaterButton from './components/TextRepeaterButton';
+
+function App() {
+  const [light, setLight] = useState('off');
+  const dark = (light === 'off') ? 'dark' : '';
+  return (
+    <div className={`App ${dark}`}>
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton></AngryButton>
+        <CounterButton></CounterButton>
+        <LightSwitchButton light={light} setLight={setLight}></LightSwitchButton>
+        <TextRepeaterButton></TextRepeaterButton>
+      </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LightSwitchButton.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function LightSwitchButton(props) {
+  const {light, setLight} = props;
+  const handleClick = () => {
+    if (light === "off") {
+      setLight("on");
+    } else {
+      setLight("off");
+    }
+  } 
+  return (
+    <button onClick={handleClick} className="LightSwitchButton">
+      {light === "on" && <span><i>💡</i> I'm on!</span>}
+      {light === "off" && <span className="off"><i>💡</i> I'm off!</span>}
+    </button>
+  );
+}
+
+export default LightSwitchButton;
